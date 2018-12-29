6e603709900b21b9f55fa7530ee44a7f554334a6
Kartik Chauhan <kckartik17@gmail.com>
2018-12-29 14:45:34 +0530

    screenshots added
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: new ThemeData(
-        brightness: Brightness.dark
+        brightness: Brightness.light
       ),
       home: MyHomePage(),
     );
